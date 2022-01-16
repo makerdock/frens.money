56ffcdf55b5aae0e2aa5c72be6ea008fd3369f6c
sanathkashyap19 <sanathkashyap92@gmail.com>
2022-01-16 16:58:45 +0530

    created basic landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,8 @@ declare let window: any;
 
 const Dashboard: React.FC = () => {
 	return (
-		<div className="min-h-screen">
-			<div className="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8 flex h-full space-x-4 items-center font-urbanist py-12">
+		<div className="max-h-screen">
+			<div className="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8 flex max-h-screen space-x-4 items-center font-urbanist py-12 pb-0">
 				<Members />
 			</div>
 		</div>
