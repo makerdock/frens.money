e8a35f508b232abb9d999b0f2634b951b7ce0201
sanathkashyap19 <sanathkashyap92@gmail.com>
2022-01-17 00:04:19 +0530

    fixed screen height

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -5,8 +5,8 @@ declare let window: any;
 
 const Dashboard: React.FC = () => {
 	return (
-		<div className="max-h-screen">
-			<div className="max-w-3xl flex justify-center items-center max-h-screen space-x-4 bg-white my-0 mx-auto py-12 pb-0">
+		<div className="min-h-screen">
+			<div className="max-w-3xl flex justify-center items-center min-h-screen space-x-4 bg-white my-0 mx-auto py-12 pb-0">
                 <div className="w-full rounded-t-3xl flex justify-center items-center space-x-4 bg-dark-black py-12">
                     <span className="text-2xl text-white font-medium">Overall, You are Owed</span>
                 </div>
