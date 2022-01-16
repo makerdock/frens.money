d2e4cdac5832aaab0efedbee339268d11451a90a
sanathkashyap19 <sanathkashyap92@gmail.com>
2022-01-16 20:59:38 +0530

    button redirect

--- components/Account.tsx
+++ components/Account.tsx
@@ -68,7 +68,7 @@ function Account() {
 			<button
 				type="button"
 				onClick={handleAuth}
-				className="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-cryptopurple hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-cryptopurple"
+				className="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-cryptopurple focus:outline-none"
 			>
 				Connect Wallet
 			</button>
